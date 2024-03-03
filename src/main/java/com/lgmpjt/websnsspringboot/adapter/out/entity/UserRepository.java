d46052c50989ba7d30c3cc60d542217e5bb3803a
lgm1007 <flxl654@gmail.com>
2024-03-03 11:38:19 +0900

    modify: UserRepository 필요 메서드 구현

--- src/main/java/com/lgmpjt/websnsspringboot/adapter/out/entity/UserRepository.java
+++ src/main/java/com/lgmpjt/websnsspringboot/adapter/out/entity/UserRepository.java
@@ -1,4 +1,11 @@
 package com.lgmpjt.websnsspringboot.adapter.out.entity;
 
+import java.util.Optional;
+
 public interface UserRepository  {
+	Optional<User> findById(Long userSeq);
+
+	User save(User user);
+
+	void delete(User user);
 }
--- src/main/java/com/lgmpjt/websnsspringboot/infra/repository/adapter/UserRepositoryAdapter.java
+++ src/main/java/com/lgmpjt/websnsspringboot/infra/repository/adapter/UserRepositoryAdapter.java
@@ -1,9 +1,12 @@
 package com.lgmpjt.websnsspringboot.infra.repository.adapter;
 
+import com.lgmpjt.websnsspringboot.adapter.out.entity.User;
 import com.lgmpjt.websnsspringboot.adapter.out.entity.UserRepository;
 import com.lgmpjt.websnsspringboot.infra.repository.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -11,4 +14,18 @@ public class UserRepositoryAdapter implements UserRepository {
 
 	private final UserJpaRepository jpaRepository;
 
+	@Override
+	public Optional<User> findById(Long userSeq) {
+		return jpaRepository.findById(userSeq);
+	}
+
+	@Override
+	public User save(User user) {
+		return jpaRepository.save(user);
+	}
+
+	@Override
+	public void delete(User user) {
+		jpaRepository.delete(user);
+	}
 }
