8dede38a467840cd53dccbd22a48d9527733f54f
lgm1007 <flxl654@gmail.com>
2024-03-03 11:33:42 +0900

    modify: BoardRepository 필요 메서드 구현

--- src/main/java/com/lgmpjt/websnsspringboot/adapter/out/entity/BoardRepository.java
+++ src/main/java/com/lgmpjt/websnsspringboot/adapter/out/entity/BoardRepository.java
@@ -1,7 +1,15 @@
 package com.lgmpjt.websnsspringboot.adapter.out.entity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BoardRepository {
+
+	Optional<Board> findById(Long boardSeq);
+
 	List<Board> findAllByUserSeq(Long userSeq);
+
+	Board save(Board board);
+
+	void delete(Board board);
 }
--- src/main/java/com/lgmpjt/websnsspringboot/infra/repository/adapter/BoardRepositoryAdapter.java
+++ src/main/java/com/lgmpjt/websnsspringboot/infra/repository/adapter/BoardRepositoryAdapter.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,7 +16,23 @@ public class BoardRepositoryAdapter implements BoardRepository {
 	private final BoardJpaRepository jpaRepository;
 
 	@Override
+	public Optional<Board> findById(Long boardSeq) {
+		return jpaRepository.findById(boardSeq);
+	}
+
+	@Override
 	public List<Board> findAllByUserSeq(Long userSeq) {
 		return jpaRepository.findAllByUserSeq(userSeq);
 	}
+
+	@Override
+	public Board save(Board board) {
+		return jpaRepository.save(board);
+	}
+
+	@Override
+	public void delete(Board board) {
+		jpaRepository.delete(board);
+	}
+
 }
