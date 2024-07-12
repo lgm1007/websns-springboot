b864933756cf16c5e248a67bd879c2ac28e349dd
lgm1007 <flxl654@gmail.com>
2024-07-12 22:47:49 +0900

    refactor: Like 엔티티 정적 팩토리 메서드로 엔티티 생성하도록

--- src/main/java/com/lgmpjt/websnsspringboot/application/port/service/LikeService.java
+++ src/main/java/com/lgmpjt/websnsspringboot/application/port/service/LikeService.java
@@ -1,7 +1,6 @@
 package com.lgmpjt.websnsspringboot.application.port.service;
 
 import com.lgmpjt.websnsspringboot.adapter.out.persistence.entity.LikeEntity;
-import com.lgmpjt.websnsspringboot.adapter.out.persistence.entity.id.LikeId;
 import com.lgmpjt.websnsspringboot.application.port.in.LikeCommandUseCase;
 import com.lgmpjt.websnsspringboot.application.port.in.LikeSearchUseCase;
 import com.lgmpjt.websnsspringboot.application.port.out.LikePort;
@@ -9,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
@@ -21,15 +19,7 @@ public class LikeService implements LikeSearchUseCase, LikeCommandUseCase {
 
 	@Override
 	public LikeEntity createLike(final Long memberSeq, final Long boardSeq) {
-		LikeEntity like = LikeEntity.builder()
-			.likeId(
-				LikeId.builder()
-					.memberSeq(memberSeq)
-					.boardSeq(boardSeq)
-					.build()
-			)
-			.createdDate(LocalDateTime.now())
-			.build();
+		LikeEntity like = LikeEntity.to(memberSeq, boardSeq);
 		return likePort.save(like);
 	}
 
